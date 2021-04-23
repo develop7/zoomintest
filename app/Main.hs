068e6a33f4644649b4867d6e7c8448032485de77
Andrei Dziahel <develop7@develop7.info>
2021-04-23 19:04:52 +0200

    complete list of tax tiers; simplify rate calculator

--- app/Main.hs
+++ app/Main.hs
@@ -15,15 +15,24 @@ calcNISTax amount = foldl' (taxCalc amount) 0 rates
     taxCalc amount' acc TaxRate {..} =
       if amount' > lower
         then case higher of
-          Nothing -> addRate acc rate amount'
+          Nothing -> addRate amount'
           Just higher' ->
             if amount' <= higher'
-              then addRate acc rate amount'
-              else addRate acc rate higher'
+              then addRate amount'
+              else addRate higher'
         else acc
-    addRate acc rate amt = acc + round (fromIntegral amt * rate)
+      where
+        addRate amt = acc + round (fromIntegral amt * rate)
 
 data TaxRate = TaxRate {lower :: Integer, higher :: Maybe Integer, rate :: Double}
 
 rates :: [TaxRate]
-rates = [TaxRate 811_560 Nothing 0.5, TaxRate 0 (Just 63_360) 0.1]
+rates =
+  [ TaxRate 811_560 Nothing 0.5,
+    TaxRate 501_960 (Just 811_560) 0.48,
+    TaxRate 240_000 (Just 501_960) 0.34,
+    TaxRate 168_000 (Just 240_000) 0.31,
+    TaxRate 108_120 (Just 168_000) 0.21,
+    TaxRate 63_360 (Just 108_120) 0.14,
+    TaxRate 0 (Just 63_360) 0.1
+  ]
