ba7dde4d051c90f207806e224d92df648cb5cd00
Andrei Dziahel <develop7@develop7.info>
2021-04-27 08:37:17 +0200

    move tax calculation to library

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,6 @@
-{-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE RecordWildCards #-}
-
 module Main where
 
-import Data.List (foldl')
+import Lib
 import System.Environment (getArgs)
 import Text.Read (readEither)
 
@@ -14,32 +11,3 @@ main = do
   putStrLn $ case amt'' of
     Right amt' -> show $ calcNISTax amt'
     Left err -> "failed to parse \"" ++ amt ++ "\": " ++ err
-
-calcNISTax :: Integer -> Integer
-calcNISTax amount = foldl' (taxCalc amount) 0 rates
-  where
-    taxCalc amount' acc TaxRate {..} =
-      if amount' > lower
-        then addRate $
-          case higher of
-            Nothing -> amount'
-            Just higher' ->
-              if amount' <= higher'
-                then amount' - lower
-                else higher'
-        else acc
-      where
-        addRate amt = acc + round (fromIntegral amt * rate)
-
-data TaxRate = TaxRate {lower :: Integer, higher :: Maybe Integer, rate :: Double}
-
-rates :: [TaxRate]
-rates =
-  [ TaxRate 811_560 Nothing 0.5,
-    TaxRate 501_960 (Just 811_560) 0.48,
-    TaxRate 240_000 (Just 501_960) 0.34,
-    TaxRate 168_000 (Just 240_000) 0.31,
-    TaxRate 108_120 (Just 168_000) 0.21,
-    TaxRate 63_360 (Just 108_120) 0.14,
-    TaxRate 0 (Just 63_360) 0.1
-  ]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,39 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE RecordWildCards #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( calcNISTax,
+    TaxRate (..),
+  )
+where
+
+import Data.List (foldl')
+
+calcNISTax :: Integer -> Integer
+calcNISTax amount = foldl' (taxCalc amount) 0 rates
+  where
+    taxCalc amount' acc TaxRate {..} =
+      if amount' > lower
+        then addRate $
+          case higher of
+            Nothing -> amount'
+            Just higher' ->
+              if amount' <= higher'
+                then amount' - lower
+                else higher'
+        else acc
+      where
+        addRate amt = acc + round (fromIntegral amt * rate)
+
+data TaxRate = TaxRate {lower :: Integer, higher :: Maybe Integer, rate :: Double}
+
+rates :: [TaxRate]
+rates =
+  [ TaxRate 811_560 Nothing 0.5,
+    TaxRate 501_960 (Just 811_560) 0.48,
+    TaxRate 240_000 (Just 501_960) 0.34,
+    TaxRate 168_000 (Just 240_000) 0.31,
+    TaxRate 108_120 (Just 168_000) 0.21,
+    TaxRate 63_360 (Just 108_120) 0.14,
+    TaxRate 0 (Just 63_360) 0.1
+  ]
